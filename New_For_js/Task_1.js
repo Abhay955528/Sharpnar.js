a76f346a4af1649010da9cc29cea169ce992cb74
Abhay955528 <123722051+Abhay955528@users.noreply.github.com>
2023-03-23 10:43:22 +0530

    add_new_item

--- New_For_js/Task_1.js
+++ New_For_js/Task_1.js
@@ -3,7 +3,4 @@ const List = document.getElementsByClassName('list-group-item');
 List[4].style.color = 'green';
 List[4].style.fontWeight = 'bold';
 
-for (let i = 0; i < List.length; i++) {
-    List[i].style.backgroundColor = '#f4f4f4';
-}
-List[2].style.background = 'green';
+
