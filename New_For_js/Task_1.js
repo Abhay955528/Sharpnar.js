e838eadf90c686557c077bf8d12cb78017f6cc40
Abhay955528 <123722051+Abhay955528@users.noreply.github.com>
2023-03-23 12:22:17 +0530

    Add_using_loop_color

--- New_For_js/Task_1.js
+++ New_For_js/Task_1.js
@@ -22,3 +22,18 @@ let h2 = document.querySelector('.container #items');
 Add_hello_word_1.append(add_tex);
 console.log(Add_hello_word_1);
 contain.insertBefore(Add_hello_word_1, h2)
+
+// task six
+
+List[1].style.color = 'green';
+
+
+let titels = document.querySelectorAll('.title');
+
+let odd = document.querySelectorAll('li:nth-child(odd)');
+let even = document.querySelectorAll('li:nth-child(even)');
+
+for (let i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = "#f4f4f4";
+    even[i].style.backgroundColor = "#ccc";
+}
