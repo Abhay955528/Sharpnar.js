50f9cf51d4a1388531d73aba34e94703ac47b6f1
Abhay955528 <123722051+Abhay955528@users.noreply.github.com>
2023-03-23 10:56:44 +0530

    new_item

--- New_For_js/Task_1.js
+++ New_For_js/Task_1.js
@@ -3,8 +3,22 @@ const List = document.getElementsByClassName('list-group-item');
 List[4].style.color = 'green';
 List[4].style.fontWeight = 'bold';
 
-// second task
+// four task
 for (let i = 0; i < List.length; i++) {
     List[i].style.backgroundColor = '#f4f4f4';
 }
 List[2].style.background = 'green';
+
+// task five
+
+let Add_hello_word_1 = document.createElement('div');
+Add_hello_word_1.id = 'item';
+let add_tex = document.createTextNode('Hello world');
+
+let contain = document.querySelector('.container #main');
+
+let h2 = document.querySelector('.container #items');
+
+Add_hello_word_1.append(add_tex);
+console.log(Add_hello_word_1);
+contain.insertBefore(Add_hello_word_1, h2)
